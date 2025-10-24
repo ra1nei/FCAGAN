17c8b890abf8510ede2ab17cd9ce618d7d91d0c4
Trần Đình Khánh Đăng <khanhdangtran2004@gmail.com>
2025-10-24 11:39:15 +0700

    Update networks.py

--- models/networks.py
+++ models/networks.py
@@ -322,10 +322,15 @@ class GANLoss(nn.Module):
             target_tensor = self.fake_label
         return target_tensor.expand_as(prediction)
 
-    def forward(self, prediction, target_is_real , train_gen=False):
+    def __call__(self, prediction, target_is_real , train_gen=False):
         if self.gan_mode in ['lsgan', 'vanilla']:
             target_tensor = self.get_target_tensor(prediction, target_is_real)
             loss = self.loss(prediction, target_tensor)
+        elif self.gan_mode in ['hinge', 'hinge2']:
+            if target_is_real:
+                loss = torch.nn.ReLU()(1.0 - prediction).mean()
+            else:
+                loss = torch.nn.ReLU()(1.0 + prediction).mean()
         elif self.gan_mode == 'smoth——hinge2':
             if train_gen:
                 loss1 = -prediction[1].mean()
