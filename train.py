9e82fd59f6b8df105672b70c94f17a3efa6f6559
Trần Đình Khánh Đăng <khanhdangtran2004@gmail.com>
2025-10-27 15:18:08 +0700

    Update train.py

--- train.py
+++ train.py
@@ -91,7 +91,7 @@ if __name__ == '__main__':
                 if total_iters % opt.print_freq == 0:
                     losses = model.get_current_losses()
                     t_comp = (time.time() - iter_start_time) / opt.batch_size
-                    visualizer.print_current_losses(epoch, epoch_iter, losses, t_comp, t_data)
+                    visualizer.print_current_losses(epoch, epoch_iter, losses, t_comp)
                     if opt.display_id > 0:
                         visualizer.plot_current_losses(epoch, float(epoch_iter) / dataset_size, losses)
 
