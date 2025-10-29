724dc8a3e6484388ea1bb1a7f4441cfce7fb1f3e
Trần Đình Khánh Đăng <khanhdangtran2004@gmail.com>
2025-10-29 10:24:40 +0700

    At least a rag can absorb water, this code can't absorb shit

--- options/test_options.py
+++ options/test_options.py
@@ -6,7 +6,7 @@ class TestOptions(BaseOptions):
     
     def initialize(self, parser):
         parser = BaseOptions.initialize(self, parser)  # define shared options
-        parser.add_argument('--results_dir', type=str, default='.results', help='saves results here.')
+        parser.add_argument('--results_dir', type=str, default='./results', help='saves results here.')
         parser.add_argument('--aspect_ratio', type=float, default=1.0, help='aspect ratio of result images')
         parser.add_argument('--phase', type=str, default='test_unknown_style', help='train, test_unknown_style, test_unknown_content, etc')
         # Dropout and Batchnorm has different behavioir during training and test.
